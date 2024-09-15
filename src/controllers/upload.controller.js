d17fda7298c912f3d3755ddde332feefb3dabf49
aviraj0403 <aviraj0403@gmail.com>
2024-09-15 18:44:06 +0530

    "Updated upload controller to remove local file after Cloudinary upload, and refactored multer storage configuration in upload middleware."

--- src/controllers/upload.controller.js
+++ src/controllers/upload.controller.js
@@ -1,4 +1,6 @@
 import cloudinary from 'cloudinary';
+import fs from 'fs';
+import path from 'path';
 import { ApiError } from '../utils/ApiError.js' // Adjust path as necessary
 import { configCloudinary } from '../config/cloudinaryConfig.js';
 // Configure Cloudinary
@@ -19,7 +21,14 @@ export const uploadImage = async (req, res, next) => {
     const result = await cloudinary.v2.uploader.upload(req.file.path, {
       folder: 'food_images',
     });
-
+    
+     // Remove file from local 'uploads' folder after upload
+     fs.unlink(req.file.path, (err) => {
+      if (err) {
+        console.error('Error deleting local file:', err);
+      }
+    });
+    
     res.status(200).json({
       imageUrl: result.secure_url
     });
--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,13 +1,12 @@
 import multer from 'multer';
 import path from 'path';
 
-// Set up multer storage
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // Destination folder
+    cb(null, 'uploads/'); // Destination directory
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname)); // File name
+    cb(null, Date.now() + path.extname(file.originalname)); // Filename with timestamp
   }
 });
 
