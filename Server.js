7268fbed9aae11de42a7d2b0c9194594f9881f0d
aviraj0403 <aviraj0403@gmail.com>
2024-09-28 10:38:50 +0530

    update

--- Server.js
+++ Server.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './src/config/databaseConfig.js';
-import app from './App.js'; // Import the Express app
+import app from './src/App.js'; // Import the Express app
 
 // Load environment variables from .env file
 
